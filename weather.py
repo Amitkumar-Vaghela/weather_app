49d73ef1e6e306a4538780e98b7e51d19ededf80
Amit vaghela <138572130+Amitkumar-Vaghela@users.noreply.github.com>
2024-04-01 21:33:21 +0530

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,41 +1,34 @@
-from flask import Flask, render_template, request 
-  
-# import json to load JSON data to a python dictionary 
-import json 
-  
-# urllib.request to make a request to api 
-import urllib.request 
-  
-  
-app = Flask(__name__) 
-  
-@app.route('/', methods =['POST', 'GET']) 
-def weather(): 
-    if request.method == 'POST': 
-        city = request.form['city'] 
-    else: 
-        # for default name mathura 
-        city = 'Baroda'
-  
-    # your API key will come here 
-    api = 
-  
-    # source contain json data from api 
-    source = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q =' + city + '&appid =' + api).read() 
-  
-    # converting JSON data to a dictionary 
-    list_of_data = json.loads(source) 
-  
-    # data for variable list_of_data 
-    data = { 
-        "country_code": str(list_of_data['sys']['country']), 
-        "coordinate": str(list_of_data['coord']['lon']) + ' ' 
-                    + str(list_of_data['coord']['lat']), 
-        "temp": str(list_of_data['main']['temp']) + 'k', 
-        "pressure": str(list_of_data['main']['pressure']), 
-        "humidity": str(list_of_data['main']['humidity']), 
-    } 
-    print(data) 
-    return render_template('index.html', data = data) 
-if __name__ == '__main__': 
-    app.run(debug = True)
+from flask import Flask, render_template, request
+import urllib.request
+import json
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def weather():
+    if request.method == 'POST':
+        city = request.form['city']
+        url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid=0a9d48239b15a060da1987896755ce39'
+        response = urllib.request.urlopen(url)
+        data = json.loads(response.read())
+
+        country_name = data['sys']['country']
+        temp_kelvin = data['main']['temp']
+        pressure = data['main']['pressure']
+        humidity = data['main']['humidity']
+        coordinate = f'{data["coord"]["lon"]}, {data["coord"]["lat"]}'
+
+        temp_celsius = round(temp_kelvin - 273.15, 1)
+
+        return render_template('index.html',
+                               data={'country_name': country_name.upper(),
+                                     'temp_celsius': temp_celsius,
+                                     'pressure': f'{pressure} hPa',
+                                     'humidity': f'{humidity}%',
+                                     'coordinate': coordinate})
+
+    return render_template('index.html', data=None)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
