8d3c8720564e339bbf62531fa265f7f21c20f869
Amit vaghela <138572130+Amitkumar-Vaghela@users.noreply.github.com>
2024-09-03 13:33:09 +0530

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,43 +1,43 @@
-from flask import Flask, render_template, request, url_for
-import requests
+from flask import Flask, render_template, request
+from flask import send_from_directory
+import urllib.request
 import json
 
 app = Flask(__name__)
-
-WEATHER_API_ENDPOINT = 'http://api.openweathermap.org/data/2.5/weather'
-API_KEY = '0a9d48239b15a060da1987896755ce39'
 
 @app.route('/', methods=['GET', 'POST'])
 def weather():
     if request.method == 'POST':
         city = request.form['city']
-        params = {
-            'q': city,
-            'appid': API_KEY,
-            'units': 'metric'  # Get temperature in Celsius
-        }
-        response = requests.get(WEATHER_API_ENDPOINT, params=params)
-        data = response.json()
+        url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid=0a9d48239b15a060da1987896755ce39'
+        response = urllib.request.urlopen(url)
+        data = json.loads(response.read())
 
-        if response.status_code == 200:
-            country_name = data['sys']['country']
-            temp_celsius = data['main']['temp']
-            pressure = data['main']['pressure']
-            humidity = data['main']['humidity']
-            coordinate = f"{data['coord']['lon']}, {data['coord']['lat']}"
+        country_name = data['sys']['country']
+        temp_kelvin = data['main']['temp']
+        pressure = data['main']['pressure']
+        humidity = data['main']['humidity']
+        coordinate = f'{data["coord"]["lon"]}, {data["coord"]["lat"]}'
 
-            weather_data = {
-                'country_name': country_name.upper(),
-                'temp_celsius': f"{temp_celsius}°C",
-                'pressure': f"{pressure} hPa",
-                'humidity': f"{humidity}%",
-                'coordinate': coordinate
-            }
-            return render_template('index.html', data=weather_data)
-        else:
-            return render_template('index.html', error="Failed to retrieve weather data")
+        temp_celsius = round(temp_kelvin - 273.15, 1)
+
+        return render_template('index.html',
+                               data={'country_name': country_name.upper(),
+                                     'temp_celsius': temp_celsius,
+                                     'pressure': f'{pressure} ',
+                                     'humidity': f'{humidity}',
+                                     'coordinate': coordinate})
 
     return render_template('index.html', data=None)
-    
+
+@app.route('/logo')
+def logo():
+    return render_template('logo.html')
+
+@app.route('/static/')
+def send_static(path):
+    return send_from_directory('static', path)
+
+
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
